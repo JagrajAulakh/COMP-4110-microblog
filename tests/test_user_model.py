96cf9a5c15aa26dea773aca52dbc73eac6c9d4d2
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-02-21 15:46:07 -0500

    Removed unittest and replaced with pytest

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
+import pytest
 from datetime import datetime, timedelta
-import unittest
 from app import create_app, db
 from app.models import User, Post
 from config import Config
@@ -8,33 +7,33 @@ from config import Config
 
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
     ELASTICSEARCH_URL = None
 
 
-class UserModelCase(unittest.TestCase):
-    def setUp(self):
+class TestUserModel:
+    @pytest.fixture(autouse=True)
+    def setup_app(self):
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-
-    def tearDown(self):
+        yield
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
     def test_password_hashing(self):
-        u = User(username='susan')
-        u.set_password('cat')
-        self.assertFalse(u.check_password('dog'))
-        self.assertTrue(u.check_password('cat'))
+        u = User(username="susan")
+        u.set_password("cat")
+        assert not u.check_password("dog")
+        assert u.check_password("cat")
 
     def test_avatar(self):
-        u = User(username='john', email='john@example.com')
-        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
-                                         'd4c74594d841139328695756648b6bd6'
-                                         '?d=identicon&s=128'))
+        u = User(username="john", email="john@example.com")
+        assert u.avatar(128) == (
+            "https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6?d=identicon&s=128"
+        )
 
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
@@ -42,22 +41,22 @@ class UserModelCase(unittest.TestCase):
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
-        self.assertEqual(u1.followed.all(), [])
-        self.assertEqual(u1.followers.all(), [])
+        assert u1.followed.all() == []
+        assert u1.followers.all() == []
 
         u1.follow(u2)
         db.session.commit()
-        self.assertTrue(u1.is_following(u2))
-        self.assertEqual(u1.followed.count(), 1)
-        self.assertEqual(u1.followed.first().username, 'susan')
-        self.assertEqual(u2.followers.count(), 1)
-        self.assertEqual(u2.followers.first().username, 'john')
+        assert u1.is_following(u2) is True
+        assert u1.followed.count() == 1
+        assert u1.followed.first().username == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().username == 'john'
 
         u1.unfollow(u2)
         db.session.commit()
-        self.assertFalse(u1.is_following(u2))
-        self.assertEqual(u1.followed.count(), 0)
-        self.assertEqual(u2.followers.count(), 0)
+        assert not u1.is_following(u2)
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
 
     def test_follow_posts(self):
         # create four users
@@ -92,11 +91,8 @@ class UserModelCase(unittest.TestCase):
         f2 = u2.followed_posts().all()
         f3 = u3.followed_posts().all()
         f4 = u4.followed_posts().all()
-        self.assertEqual(f1, [p2, p4, p1])
-        self.assertEqual(f2, [p2, p3])
-        self.assertEqual(f3, [p3, p4])
-        self.assertEqual(f4, [p4])
+        assert f1 == [p2, p4, p1]
+        assert f2 == [p2, p3]
+        assert f3 == [p3, p4]
+        assert f4 == [p4]
 
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
