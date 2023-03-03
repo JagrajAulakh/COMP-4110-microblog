bbb1e68b81c1188df3a8920e1524223968f9ca07
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-03-03 00:36:52 -0500

    Added tests for User model. Got to 79% coverage in models.py

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,6 @@
+import jwt
 import pytest
+from pytest_mock import mocker
 from datetime import datetime, timedelta
 from app import create_app, db
 from app.models import User, Post
@@ -16,12 +18,21 @@ class TestUserModel:
     def setup_app(self):
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
+        self.app_text_request_context = self.app.test_request_context()
         self.app_context.push()
+        self.app_text_request_context.push()
         db.create_all()
         yield
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
+
+    @pytest.fixture()
+    def new_user(self):
+        u = User(username="joe", email="joe@joe.com")
+        db.session.add(u)
+        db.session.commit()
+        yield u
 
     def test_password_hashing(self):
         u = User(username="susan")
@@ -95,4 +106,109 @@ class TestUserModel:
         assert f2 == [p2, p3]
         assert f3 == [p3, p4]
         assert f4 == [p4]
+
+    def test_get_reset_password_token_valid_secret(self, new_user):
+        password = "password"
+        new_user.set_password(password)
+
+        reset_token = new_user.get_reset_password_token()
+
+        assert reset_token
+        data = jwt.decode(reset_token, TestConfig.SECRET_KEY, algorithms=["HS256"])
+        assert data.get('reset_password', None) == new_user.id
+
+    def test_get_reset_password_token_invalid_secret(self, new_user):
+        reset_token = new_user.get_reset_password_token()
+        try:
+            _ = jwt.decode(reset_token, 'not_so_secret_key...also_invalid', algorithms=["HS256"])
+            assert False
+        except jwt.InvalidSignatureError:
+            assert True
+
+    def test_verify_reset_password_token_valid_secret(self, new_user):
+        encoded_token = jwt.encode(
+                {'reset_password': new_user.id}, TestConfig.SECRET_KEY, algorithm="HS256")
+        
+        user = new_user.verify_reset_password_token(encoded_token)
+        assert user.id == new_user.id
+
+    def test_verify_reset_password_token_invalid_secret(self, new_user):
+        encoded_token = jwt.encode(
+                {'reset_password': new_user.id}, 'not_so_secret_key...also_invalid', algorithm="HS256")
+        user = new_user.verify_reset_password_token(encoded_token)
+        assert user is None
+
+    def test_user_from_dict_with_password(self, new_user):
+        user_dict = {
+                'username': 'joe',
+                'email': 'joe@joe.com',
+                'about_me': 'Hi, I\'m Joe... Joe MAMA!',
+                'password': 'super_secret_password'
+                }
+
+        new_user.from_dict(user_dict, new_user=True)
+
+        assert new_user.username == user_dict['username']
+        assert new_user.email == user_dict['email']
+        assert new_user.check_password(user_dict['password'])
+
+
+    def test_user_from_dict_no_password(self, new_user):
+        user_dict = {
+                'username': 'joe',
+                'email': 'joe@joe.com',
+                'about_me': 'Hi, I\'m Joe... Joe MAMA!',
+                }
+
+        new_user.from_dict(user_dict)
+
+        assert new_user.username == user_dict['username']
+        assert new_user.email == user_dict['email']
+        assert new_user.password_hash is None
+
+    def test_user_to_dict(self, new_user):
+        data = new_user.to_dict(include_email=True)
+
+        assert type(data) == dict
+
+        assert data['id'] == new_user.id
+        assert data['username'] == new_user.username
+        assert data['email'] == new_user.email
+        assert data['about_me'] == new_user.about_me
+
+    def test_user_get_token(self, new_user: User):
+        assert new_user.token is None
+        token = new_user.get_token()
+        assert token is not None
+        new_token = new_user.get_token()
+        assert token is not None
+        assert new_token == token
+        assert datetime.utcnow() < new_user.token_expiration
+
+    def test_user_revoke_token(self, mocker, new_user: User):
+        TOKEN = 'thisisthetoken'
+        mocker.patch('app.models.User.get_token', return_value=TOKEN)
+        token = new_user.get_token()
+        assert token == TOKEN
+
+        new_user.revoke_token()
+        assert datetime.utcnow() > new_user.token_expiration
+
+    def test_user_check_token_valid(self, new_user):
+        token = new_user.get_token()
+        assert User.check_token(token).id == new_user.id
+
+    def test_user_check_token_invalid(self, new_user):
+        _ = new_user.get_token()
+        assert User.check_token('garbage') is None
+
+    def test_user_check_token_expired(self, mocker, new_user):
+        TOKEN = 'thisisthetoken'
+        mocker.patch.object(User, 'token', TOKEN)
+        mocker.patch('app.models.User.get_token', return_value=TOKEN)
+        new_user.token_expiration = datetime.utcnow()-timedelta(seconds=1)
+
+        assert User.check_token(new_user.get_token()) is None
+
+
 
