13183016cec9d9c13b78168378aba206468c51c9
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-03-28 14:51:07 -0400

    #96 Modified test to login with a test user and send get request to the user's profile page

--- tests/performance/locust.py
+++ tests/performance/locust.py
@@ -1,17 +1,26 @@
-import time
+import pyotp
 
 from locust import HttpUser, task, between
+
 
 class PerfUser(HttpUser):
     wait_time = between(1, 2)
 
     @task
     def visit_account_page(self):
-        self.client.get("/auth/login")
-
+        self.client.get("/user/testuser")
 
     def on_start(self):
-        # self.client.post("/login", json={"username":"foo", "password":"bar"})
-        pass
+        credentials = {
+            "username": "testuser",
+            "userid": 1,
+            "password": "testuser",
+            "secret": "5D2PMXTITWZHC4K5JUGGIKHOKAZJRAD2",  # Secret for testuser on production site
+        }
+        totp = pyotp.TOTP(credentials["secret"])
 
+        otp_code = totp.now()
 
+        self.client.post(
+            f"/auth/login/2fa/{credentials['userid']}", data={"otp": otp_code}
+        )
